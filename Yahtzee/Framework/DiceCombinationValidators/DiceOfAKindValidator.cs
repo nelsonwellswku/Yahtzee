c01358240455529836be4852f6b3086b0bf01fb0
Nelson Wells <nelsonwellswku@gmail.com>
2013-03-02 01:23:52 -0600

    Three of a kind validator and tests

---
+++ Yahtzee/Framework/DiceCombinationValidators/DiceOfAKindValidator.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace Yahtzee.Framework.DiceCombinationValidators
+{
+	public class DiceOfAKindValidator : IDiceOfAKindValidator
+	{
+		public bool IsValid(int numberOfDiceToMatch, IEnumerable<int> dice)
+		{
+			foreach (var uniqueValue in dice.Distinct())
+			{
+				if (dice.Count(x => x == uniqueValue) == numberOfDiceToMatch)
+				{
+					return true;
+				}
+			}
+
+			return false;
+		}
+	}
+}
---
+++ YahtzeeTests/DieCupValidatorTests.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+using FluentAssertions;
+using Yahtzee.Framework.DiceCombinationValidators;
+
+namespace YahtzeeTests
+{
+	[TestClass]
+	public class DieCupValidatorTests
+	{
+		[TestMethod]
+		public void ValidateThreeOfAKind()
+		{
+			int[] dice = { 3, 4, 3, 5, 3 };
+
+			DiceOfAKindValidator diceOfAKindValidator = new DiceOfAKindValidator();
+			bool result = diceOfAKindValidator.IsValid(3, dice);
+			result.Should().BeTrue();
+		}
+
+		[TestMethod]
+		public void ValidateInvalidThreeOfAKind()
+		{
+			int[] dice = { 3, 1, 5, 3, 4 };
+
+			DiceOfAKindValidator diceOfAKindValidator = new DiceOfAKindValidator();
+			bool result = diceOfAKindValidator.IsValid(3, dice);
+			result.Should().BeFalse();
+		}
+	}
+}
