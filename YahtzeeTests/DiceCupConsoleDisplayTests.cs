5afc2dd933ffb3d84b9955fb027ab275def61d3d
Nelson Wells <nelsonwellswku@gmail.com>
2013-05-06 22:57:19 -0500

    Display dice cup to console

---
+++ YahtzeeTests/DiceCupConsoleDisplayTests.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+using Moq;
+using FluentAssertions;
+using Yahtzee.Framework;
+using YahtzeeTests.Support;
+using System.Text;
+
+namespace YahtzeeTests
+{
+	[TestClass]
+	public class DiceCupConsoleDisplayTests
+	{
+		private const string _prompt = "Your current dice cup contents:";
+
+		string _actual;
+		string _expected;
+
+		Mock<IDiceCup> _diceCupMock;
+		TestDieFactory _testDieFactory;
+
+		IDiceCupConsoleDisplay _diceCupConsoleDisplay;
+
+		public DiceCupConsoleDisplayTests()
+		{
+			_testDieFactory = new TestDieFactory();
+			_diceCupMock = new Mock<IDiceCup>();
+		}
+
+		[TestMethod]
+		public void DiceCupConsoleDisplay_DiceCupHasSetOfOnes_DisplayOnConsole()
+		{
+			// Arrange
+			_diceCupMock.Setup(x => x.Dice).Returns(_testDieFactory.CreateDieEnumerable(1, 1, 1, 1, 1));
+			_expected = _prompt + "\r\n" + "| 1 | | 1 | | 1 | | 1 | | 1 |";
+
+			//Act
+			using (var capturer = new ConsoleCapturer())
+			{
+				_diceCupConsoleDisplay = new DiceCupConsoleDisplay();
+				_diceCupConsoleDisplay.Show(_diceCupMock.Object);
+				_actual = capturer.GetStandardOut();
+			}
+
+			// Assert
+			_actual.Should().Be(_expected);
+		}
+
+		[TestMethod]
+		public void DiceCupConsoleDisplay_DIceCupHasRandomCollection_DisplayOnConsole()
+		{
+			// Arrange
+			_diceCupMock.Setup(x => x.Dice).Returns(_testDieFactory.CreateDieEnumerable(1, 5, 3, 4, 6));
+			_expected = _prompt + "\r\n" + "| 1 | | 5 | | 3 | | 4 | | 6 |";
+
+			// Act
+			using (var capturer = new ConsoleCapturer())
+			{
+				_diceCupConsoleDisplay = new DiceCupConsoleDisplay();
+				_diceCupConsoleDisplay.Show(_diceCupMock.Object);
+				_actual = capturer.GetStandardOut();
+			}
+
+			// Assert
+			_actual.Should().Be(_expected);
+		}
+	}
+
+	public interface IDiceCupConsoleDisplay
+	{
+		void Show(IDiceCup diceCup);
+	}
+
+	public class DiceCupConsoleDisplay : IDiceCupConsoleDisplay
+	{
+		public const string _prompt = "Your current dice cup contents:\r\n";
+
+		public void Show(IDiceCup diceCup)
+		{
+			StringBuilder sb = new StringBuilder(_prompt);
+			foreach (var die in diceCup.Dice)
+			{
+				sb.Append("| ");
+				sb.Append(die.Value);
+				sb.Append(" | ");
+			}
+			Console.Write(sb.ToString().Trim());
+		}
+	}
+}
---
+++ YahtzeeTests/Support/ConsoleCapturer.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace YahtzeeTests.Support
+{
+	class ConsoleCapturer : IDisposable
+	{
+		private TextWriter _previousOut;
+		private StringWriter _capturer;
+
+		public ConsoleCapturer()
+		{
+			_capturer = new StringWriter();
+			_previousOut = Console.Out;
+			Console.SetOut(_capturer);
+		}
+
+		public string GetStandardOut()
+		{
+			return _capturer.ToString();
+		}
+
+		public void Dispose()
+		{
+			Console.SetOut(_previousOut);
+		}
+	}
+}
